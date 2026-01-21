06aaf875f1b22f42c5dc85f4e82bb4e9ddfecffa
CALVlN <36027062+adinnelson@users.noreply.github.com>
2026-01-20 17:36:58 -0800

    Add test player with basic movement and test scene
    - Add PlayerMoveTest scene
    - Add PlayerMovement.cs
    - Add Player prefab

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float acceleration = 1.25f;
+    [SerializeField] private float maxSpeed = 5f;
+    [SerializeField] private float drag = 1f;
+
+    private InputSystem_Actions inputActions;
+    private new Rigidbody2D rigidbody;
+
+    void OnDisable() {
+        inputActions.Player.Disable();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        inputActions = new InputSystem_Actions();
+        rigidbody = GetComponent<Rigidbody2D>();
+        inputActions.Player.Enable();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        MovePlayer();
+
+        // Apply drag when no input is detected
+        if (inputActions.Player.Move.ReadValue<Vector2>() == Vector2.zero)
+        {
+            ApplyDrag();
+        }
+    }
+
+    private void ApplyDrag()
+    {
+        Vector2 dragForce = -rigidbody.linearVelocity.normalized * drag;
+        if (dragForce.magnitude > rigidbody.linearVelocity.magnitude)
+        {
+            rigidbody.linearVelocity = Vector2.zero;
+        }
+        else
+        {
+            rigidbody.AddForce(dragForce, ForceMode2D.Impulse);
+        }
+    }
+
+    private void MovePlayer()
+    {
+        float moveHorizontal = inputActions.Player.Move.ReadValue<Vector2>().x;
+        float moveVertical = inputActions.Player.Move.ReadValue<Vector2>().y;
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        Vector2 moveForce = movement * acceleration;
+
+        rigidbody.AddForce(moveForce, ForceMode2D.Impulse);
+
+        // Clamp player speed to maxSpeed
+        if (rigidbody.linearVelocity.magnitude > maxSpeed)
+        {
+            rigidbody.linearVelocity = rigidbody.linearVelocity.normalized * maxSpeed;
+        }
+    }
+}
