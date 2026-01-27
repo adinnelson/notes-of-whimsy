7db408716359e69a456baef847c3b16fab8237af
Taylor <tdewjones@gmail.com>
2026-01-27 12:09:27 -0800

    Icons to match four spells accounted for in pickup script
    TODO: icons should be castable spells -- requires tie in with the attack system NOT THIS SPRINT period

--- Assets/Scripts/PickupObject.cs
+++ Assets/Scripts/PickupObject.cs
@@ -6,25 +6,47 @@ public class pickupObject : MonoBehaviour
 {
     [Header("Inventory UI")]
     [SerializeField] private Transform contentsParent;
-    [SerializeField] private GameObject templateIcon;
+    /*
+     * these "Icons for Note Type" private GameObjects are stand-ins for the actual note prefabs. 
+     * if we swap these later, other updates must also occur: 
+     *      1. prefab reference in Inspector needs updating
+     *      2. below, SpawnIcon() switch-mapping (ex. ItemType.Green => greenIcon); 
+     *      3. in ItemType.cs enum if new note types are added or changed (ex. Green)
+     */
+    [Header("Icons for Note Type")]
+    [SerializeField] private GameObject greenIcon;
+    [SerializeField] private GameObject blueIcon;
+    [SerializeField] private GameObject yellowIcon;
+    [SerializeField] private GameObject pinkIcon;
 
     private void OnTriggerEnter2D(Collider2D objToPickup)
     {
-        Debug.Log("Trigger entered by: " + objToPickup.name);
-
-        if (!objToPickup.CompareTag("puObj"))
+        var pickup = objToPickup.GetComponent<PickupItem>();
+        if (pickup == null)
         {
             return;
         }
 
-        SpawnIcon();
-        //destroy the object from the world
+        SpawnIcon(pickup.itemType);
         Destroy(objToPickup.gameObject);
     }
 
-    void SpawnIcon()
+    void SpawnIcon(ItemType type)
     {
-        GameObject newIconFromTemplate = Instantiate(templateIcon, contentsParent);
+        GameObject prefab = type switch
+        {
+            ItemType.Green => greenIcon,
+            ItemType.Blue => blueIcon,
+            ItemType.Yellow => yellowIcon,
+            ItemType.Pink => pinkIcon,
+            _ => null
+        };
+
+        if (prefab == null)
+        {
+            return;
+        }
+        GameObject newIconFromTemplate = Instantiate(prefab, contentsParent);
         newIconFromTemplate.SetActive(true);
     }
 }
