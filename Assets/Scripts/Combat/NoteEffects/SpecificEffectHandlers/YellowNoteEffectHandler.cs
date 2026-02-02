47fe43cab57f94bef3b91933d2fc7abb9f1d5ba2
LegendTova <tdcmax10@gmail.com>
2026-02-02 14:19:19 -0800

    Remove empty Start and FixedUpdate

--- Assets/Scripts/Combat/NoteEffects/SpecificEffectHandlers/YellowNoteEffectHandler.cs
+++ Assets/Scripts/Combat/NoteEffects/SpecificEffectHandlers/YellowNoteEffectHandler.cs
@@ -2,17 +2,6 @@ using UnityEngine;
 
 public class YellowNoteEffectHandler : NoteEffectHandler
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    void FixedUpdate()
-    {
-        
-    }
-
     public override void Fire()
     {
         playerAttack.FireProjectile(note, this);
