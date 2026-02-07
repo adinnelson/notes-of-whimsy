10ca053c15624e1be21e5fe89dfcd55038cb4b8a
LegendTova <tdcmax10@gmail.com>
2026-02-06 20:44:50 -0800

    Initial Chain Damage (No graphics)

--- Assets/Scripts/Combat/NoteEffects/SpecificEffectHandlers/YellowNoteEffectHandler.cs
+++ Assets/Scripts/Combat/NoteEffects/SpecificEffectHandlers/YellowNoteEffectHandler.cs
@@ -1,14 +1,67 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class YellowNoteEffectHandler : NoteEffectHandler
 {
+    float detectionRadius = 2.5f;
+
+    float timeElapsed = 0.0f;
+
+    float damage = 25f;
+
+    bool lightingEffectInProgress = false;
+
+    void FixedUpdate() 
+    {
+        if (!lightingEffectInProgress) return;
+        if(enemies.Count <= 0)
+        {
+            lightingEffectInProgress = false;
+            return;
+        }
+
+
+        if(timeElapsed >= 1.0f)
+        {
+            enemies[0].GetComponent<Health>().TakeDamage(damage);
+            enemies.RemoveAt(0);
+            timeElapsed = 0;
+        }
+
+        timeElapsed +=  Time.fixedDeltaTime;
+    }
+
     public override void Fire()
     {
         playerAttack.FireProjectile(note, this);
     }
 
+    List<EnemyBase> enemies = new List<EnemyBase>();
     public override void HitEnemy(IDamageable damageable)
     {
+        enemies.Clear();
+        
+        Health enemyHealth = (Health)damageable;
 
+        
+        Collider2D[] hitColliders = Physics2D.OverlapCircleAll(enemyHealth.transform.position, detectionRadius);
+
+        Debug.Log(hitColliders.Length);
+
+        foreach(Collider2D collider in hitColliders)
+        {
+            if(collider.gameObject == this) continue;
+
+            EnemyBase enemy = collider.gameObject.GetComponent<EnemyBase>();
+
+            if(enemy == null) continue;
+
+            enemies.Add(enemy);
+
+            lightingEffectInProgress = true;
+        }
+
+        
     }
 }
