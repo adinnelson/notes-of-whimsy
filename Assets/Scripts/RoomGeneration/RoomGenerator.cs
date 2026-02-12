f1794b4a8c95a03d61ae5ca9fe3c9669e76f5949
CALVlN <36027062+adinnelson@users.noreply.github.com>
2026-02-11 21:16:16 -0800

    Comment out temp procedural generation console prints

--- Assets/Scripts/RoomGeneration/RoomGenerator.cs
+++ Assets/Scripts/RoomGeneration/RoomGenerator.cs
@@ -270,7 +270,7 @@ idea is to make it work for any size and shape room to have creativity and relea
         int enemyCount = Random.Range(minEnemies,maxEnemies);
         // HACK: choose a random number of enemies to add to the rooms list of enemies
         Tilemap floor = currentRoom.GetFloor();
-        print(floor.cellBounds.center);
+        // print(floor.cellBounds.center);
 
         if (currentRoom.GetSafety() == false)
         {
