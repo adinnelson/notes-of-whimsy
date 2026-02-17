d50dc45b11d3676d3a88b826c4e4255baa085064
LegendTova <tdcmax10@gmail.com>
2026-02-17 14:28:28 -0800

    Create Game Manager with timers

---
+++ Assets/Scripts/SimpleTimer.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class SimpleTimer
+{
+    float duration = 60;
+    float timeRemaining;
+    bool on = false;
+    bool loop = false;
+
+    Action onFinish;
+
+    public void UpdateTimer(float time)
+    {
+        if (!on) return; 
+        
+        timeRemaining -= time;
+
+        if (timeRemaining <= 0)
+        {
+            if (onFinish != null) onFinish.Invoke();
+
+            if(loop)
+            {
+                timeRemaining = duration;
+                return;
+            }
+
+            on = false;
+        }
+    }
+
+    public void StartTimer(float duration, bool loop = false, Action onFinish = null, GameManager gameManager = null)
+    {
+        this.duration = duration;
+        timeRemaining = this.duration;
+
+        this.loop = loop;
+        this.onFinish = onFinish;
+
+        on = true;
+
+        if (gameManager == null)
+        {
+            gameManager = GameObject.FindWithTag("GameManager")?.GetComponent<GameManager>();
+            gameManager?.AddTimer(this);
+        }
+
+    }
+
+    public void Stop()
+    {
+        on = false;
+    }
+
+    public void Resume()
+    {
+        on = true;
+    }
+
+    public void Restart()
+    {
+        timeRemaining = duration;
+        on = true;
+    }
+}
