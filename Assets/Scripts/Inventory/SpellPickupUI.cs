af4e191f844ec5103a67b2f6b27dc0fd87693e28
Taylor <tdewjones@gmail.com>
2026-02-25 11:33:57 -0800

    pickup on click menu appears on the item
    TODO: limit the pickup range

--- Assets/Scripts/Inventory/SpellPickupUI.cs
+++ Assets/Scripts/Inventory/SpellPickupUI.cs
@@ -20,6 +20,11 @@ public class SpellPickupUI : MonoBehaviour
     {
         Debug.Log($"Showing pickup UI for: {pickup.spell.name}");
         currentPickup = pickup;
+
+        //set the pop-up menu to appear by the object we are picking up
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(pickup.transform.position);
+        panel.transform.position = screenPos;
+
         panel.SetActive(true);
     }
 
@@ -34,7 +39,7 @@ public class SpellPickupUI : MonoBehaviour
         {
             Debug.LogWarning("Pickup called but currentPickup is null!");
         }
-            panel.SetActive(false);
+        panel.SetActive(false);
         currentPickup = null;
     }
 
