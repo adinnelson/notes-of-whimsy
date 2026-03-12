1915405d52e6ddaf18a7d96d3843d0bae10fc5fb
Avery Simmons <130499829+AverySimmons@users.noreply.github.com>
2026-03-11 18:24:05 -0700

    deprecate old dependants
    commented out beat item and child beat spawner scripts

--- Assets/Scripts/BeatBar/BeatItem.cs
+++ Assets/Scripts/BeatBar/BeatItem.cs
@@ -4,61 +4,61 @@ using UnityEngine.Pool;
 
 public class BeatItem : MonoBehaviour
 {
-    private const float LATE_OFFSET = 0.5f; 
+    //private const float LATE_OFFSET = 0.5f; 
 
-    // reference to beat handler in in scene
-    private BeatHandler beatHandler;
+    //// reference to beat handler in in scene
+    //private BeatHandler beatHandler;
 
-    // end gameobject
-    private GameObject endGoal;
+    //// end gameobject
+    //private GameObject endGoal;
 
-    private int beatId = 0;
-    public int BeatId
-    {
-        get { return beatId; }
-    }
+    //private int beatId = 0;
+    //public int BeatId
+    //{
+    //    get { return beatId; }
+    //}
 
-    private bool unlocked = false;
-    public bool Unlocked
-    {
-        get { return unlocked;}
-        set { unlocked = value; }
-    }
+    //private bool unlocked = false;
+    //public bool Unlocked
+    //{
+    //    get { return unlocked;}
+    //    set { unlocked = value; }
+    //}
 
-    float stepSize = 0.0f;
+    //float stepSize = 0.0f;
 
-    void FixedUpdate()
-    {
-        if(endGoal == null) return;
+    //void FixedUpdate()
+    //{
+    //    if(endGoal == null) return;
 
-        if(this.gameObject.transform.position.x <= endGoal.transform.position.x - LATE_OFFSET)
-        {
-            beatHandler.BeatArrived(this);
-            return;
-        }
+    //    if(this.gameObject.transform.position.x <= endGoal.transform.position.x - LATE_OFFSET)
+    //    {
+    //        beatHandler.BeatArrived(this);
+    //        return;
+    //    }
 
-        this.transform.localPosition += Vector3.left * stepSize;
-    }
+    //    this.transform.localPosition += Vector3.left * stepSize;
+    //}
 
-    // Initializes beat item
-    public void Init(BeatHandler beatHandler, Color colour, GameObject endGoal, Vector3 position, float stepSize, int tickId, bool unlocked = false)
-    {
-        // references used
-        this.beatHandler = beatHandler;
-        this.endGoal = endGoal;
+    //// Initializes beat item
+    //public void Init(BeatHandler beatHandler, Color colour, GameObject endGoal, Vector3 position, float stepSize, int tickId, bool unlocked = false)
+    //{
+    //    // references used
+    //    this.beatHandler = beatHandler;
+    //    this.endGoal = endGoal;
 
-        // sets initial position
-        this.transform.position = position;
+    //    // sets initial position
+    //    this.transform.position = position;
 
-        // amount to move after Time.fixedDeltaTime
-        this.stepSize = stepSize;
+    //    // amount to move after Time.fixedDeltaTime
+    //    this.stepSize = stepSize;
 
-        // set color
-        GetComponent<SpriteRenderer>().color = colour;
+    //    // set color
+    //    GetComponent<SpriteRenderer>().color = colour;
 
-        this.beatId = tickId;
+    //    this.beatId = tickId;
 
-        this.unlocked = unlocked;
-    }
+    //    this.unlocked = unlocked;
+    //}
 
 }
--- Assets/Scripts/BeatBar/ChildBeatSpawner.cs
+++ Assets/Scripts/BeatBar/ChildBeatSpawner.cs
@@ -2,47 +2,47 @@ using UnityEngine;
 
 public class ChildBeatSpawner : MonoBehaviour
 {
-    private float spawnTime = 0.0f;
-    public float SpawnTime
-    {
-        get { return spawnTime; }
-    }
+    //private float spawnTime = 0.0f;
+    //public float SpawnTime
+    //{
+    //    get { return spawnTime; }
+    //}
 
-    private float timeElapsed = 0.0f;
-    private bool waitingToSpawn = false;
+    //private float timeElapsed = 0.0f;
+    //private bool waitingToSpawn = false;
 
-    private BeatHandler beatHandler;
-    private Color colour;
-    public Color Colour 
-    {
-        get { return colour; }
-    }
+    //private BeatHandler beatHandler;
+    //private Color colour;
+    //public Color Colour 
+    //{
+    //    get { return colour; }
+    //}
     
-    void FixedUpdate() 
-    {
-        if(!waitingToSpawn) return;
+    //void FixedUpdate() 
+    //{
+    //    if(!waitingToSpawn) return;
 
-        if (timeElapsed >= spawnTime)
-        {
-            beatHandler.SpawnAdditionalBeat(colour);
-            timeElapsed = 0.0f;
-            waitingToSpawn = false;
-        }
+    //    if (timeElapsed >= spawnTime)
+    //    {
+    //        beatHandler.SpawnAdditionalBeat(colour);
+    //        timeElapsed = 0.0f;
+    //        waitingToSpawn = false;
+    //    }
 
-        timeElapsed += Time.fixedDeltaTime;
-    }
+    //    timeElapsed += Time.fixedDeltaTime;
+    //}
 
-    public void Init(BeatHandler beatHandler, Color colour, Vector3 position, float spawnTime)
-    {
-        this.beatHandler = beatHandler;
-        this.transform.position = position;
-        this.spawnTime = spawnTime;
-        this.colour = colour;
-    }
+    //public void Init(BeatHandler beatHandler, Color colour, Vector3 position, float spawnTime)
+    //{
+    //    this.beatHandler = beatHandler;
+    //    this.transform.position = position;
+    //    this.spawnTime = spawnTime;
+    //    this.colour = colour;
+    //}
 
-    // Called when main beat item reaches end
-    public void StartSpawnCountdown()
-    {
-        waitingToSpawn = true;
-    }
+    //// Called when main beat item reaches end
+    //public void StartSpawnCountdown()
+    //{
+    //    waitingToSpawn = true;
+    //}
 }
