45b4e3ec11ae4c09abbc0878725b270e8c98bf14
Amir Alrishan <159516091+DarknessTookMe@users.noreply.github.com>
2026-04-07 23:25:07 -0700

    Revert "Update PlayerAttack.cs"
    This reverts commit 9daabf80e2894289340e2261fb770876441e99da.

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -134,10 +134,14 @@ public class PlayerAttack : MonoBehaviour
             animator.SetTrigger("Dash");
             dashInvulnerableUntil = Time.time + dashInvulnerabilityDuration;
             Rigidbody2D rb = GetComponent<Rigidbody2D>();
+<<<<<<< HEAD
+            if (rb.linearVelocity.magnitude > 0)
+=======
 
             animator.SetTrigger("Dash");
 
             if(rb.linearVelocity.magnitude > 0)
+>>>>>>> main
             {
 
                 rb.AddForce(rb.linearVelocity.normalized * 3000);
@@ -147,8 +151,13 @@ public class PlayerAttack : MonoBehaviour
                 Vector3 mouseScreenPosition = Mouse.current.position.value;
                 Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(mouseScreenPosition);
                 Vector2 direction = new Vector2(mouseWorldPosition.x - transform.position.x, mouseWorldPosition.y - transform.position.y);
+<<<<<<< HEAD
+                rb.AddForce(direction.normalized * 3000);
+
+=======
 
                 rb.AddForce(direction.normalized * 3000);   
+>>>>>>> main
             }
             return;
         }
